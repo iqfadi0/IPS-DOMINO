7b74f3ae2a3974f5ea92cd743f5fed3b016e0c63
iqfadi0 <iqxf40@gmail.com>
2025-07-29 22:17:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,6 @@ def change_password():
         return redirect("/dashboard")
     return render_template("change_password.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
